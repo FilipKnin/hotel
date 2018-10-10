3db28c7c98d429b2f00b486f7b5173dd03fbcc40
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-10 11:49:30 +0100

    finishes MVP

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,79 @@
+import java.util.ArrayList;
+
 public class Hotel {
+    private String name;
+    private ArrayList<Bedroom> bedroomList;
+    private ArrayList<DiningRoom> diningRoomList;
+    private ArrayList<ConferenceRoom> conferenceRoomList;
+
+    public Hotel(String name){
+        this.name = name;
+        this.bedroomList = new ArrayList<>();
+        this.diningRoomList = new ArrayList<>();
+        this.conferenceRoomList = new ArrayList<>();
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void addBedroom(Bedroom bedroom){
+        bedroomList.add(bedroom);
+    }
+
+    public void addDiningRoom(DiningRoom diningRoom){
+        diningRoomList.add(diningRoom);
+    }
+
+    public void addConferenceRoom(ConferenceRoom conferenceRoom){
+        conferenceRoomList.add(conferenceRoom);
+    }
+
+
+    public ArrayList<Bedroom> getBedroomList() {
+        return bedroomList;
+    }
+
+    public ArrayList<DiningRoom> getDiningRoomList() {
+        return diningRoomList;
+    }
+
+    public ArrayList<ConferenceRoom> getConferenceRoomList() {
+        return conferenceRoomList;
+    }
+
+
+    public void checkInBedroom(Guest guest, Bedroom bedroom) {
+        bedroom.addGuest(guest);
+    }
+
+    public void checkOutBedroom(Bedroom bedroom){
+        bedroom.clearRoom();
+    }
+
+
+    public void checkInDiningRoom(Guest guest, DiningRoom diningRoom) {
+        diningRoom.addGuest(guest);
+    }
+
+    public void checkOutDiningRoom(DiningRoom diningRoom) {
+        diningRoom.removeGuest();
+    }
+
+    public void clearDiningRoom(DiningRoom diningRoom){
+        diningRoom.clearRoom();
+    }
+
+    public void checkInConferenceRoom(Guest guest, ConferenceRoom conferenceRoom) {
+        conferenceRoom.addGuest(guest);
+    }
+
+    public void checkOutConferenceRoom(ConferenceRoom conferenceRoom) {
+        conferenceRoom.removeGuest();
+    }
+
+    public void clearConferenceRoom(ConferenceRoom conferenceRoom) {
+        conferenceRoom.clearRoom();
+    }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,121 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HotelTest {
+    Hotel hotel;
+    DiningRoom diningRoom;
+    ConferenceRoom conferenceRoom;
+    Bedroom bedroom;
+    Guest guest;
+
+    @Before
+    public void beforeEach() {
+        hotel = new Hotel("Fluffy Towers");
+        bedroom = new Bedroom(10,2,"DBL");
+        conferenceRoom = new ConferenceRoom("The Hall", 200);
+        diningRoom = new DiningRoom("The Egg Lounge", 50);
+        guest = new Guest();
+    }
+
+    @Test
+    public void getName(){
+        assertEquals("Fluffy Towers", hotel.getName() );
+    }
+
+    @Test
+    public void getBedroomList(){
+        assertEquals(0, hotel.getBedroomList().size() );
+    }
+
+    @Test
+    public void addBedrooms(){
+        hotel.addBedroom(bedroom);
+        assertEquals(1, hotel.getBedroomList().size() );
+    }
+
+    @Test
+    public void getDiningRoomList(){
+        assertEquals(0, hotel.getDiningRoomList().size() );
+    }
+
+    @Test
+    public void addDiningRoom(){
+        hotel.addDiningRoom(diningRoom);
+        assertEquals(1, hotel.getDiningRoomList().size() );
+    }
+
+    @Test
+    public void getConferenceRoomList(){
+        assertEquals(0, hotel.getConferenceRoomList().size() );
+    }
+
+    @Test
+    public void addConferenceRoom(){
+        hotel.addConferenceRoom(conferenceRoom);
+        assertEquals(1, hotel.getConferenceRoomList().size() );
+    }
+
+    @Test
+    public void checkInBedroom(){
+        hotel.checkInBedroom(guest, bedroom);
+        assertEquals(1, bedroom.getGuestList().size() );
+    }
+
+    @Test
+    public void checkOutBedroom(){
+        hotel.checkInBedroom(guest, bedroom);
+        assertEquals(1, bedroom.getGuestList().size() );
+        hotel.checkOutBedroom(bedroom);
+        assertEquals(0, bedroom.getGuestList().size() );
+    }
+
+    @Test
+    public void checkInDiningRoom(){
+        hotel.checkInDiningRoom(guest, diningRoom);
+        assertEquals(1, diningRoom.getGuestList().size() );
+    }
+
+    @Test
+    public void checkOutDiningRoom(){
+        hotel.checkInDiningRoom(guest, diningRoom);
+        assertEquals(1, diningRoom.getGuestList().size() );
+        hotel.checkOutDiningRoom(diningRoom);
+        assertEquals(0, diningRoom.getGuestList().size() );
+    }
+
+    @Test
+    public void clearDiningRoom(){
+        hotel.checkInDiningRoom(guest, diningRoom);
+        hotel.checkInDiningRoom(guest, diningRoom);
+        hotel.checkInDiningRoom(guest, diningRoom);
+        assertEquals(3, diningRoom.getGuestList().size() );
+        hotel.clearDiningRoom(diningRoom);
+        assertEquals(0, diningRoom.getGuestList().size() );
+    }
+
+    @Test
+    public void checkInConferenceRoom(){
+        hotel.checkInConferenceRoom(guest, conferenceRoom);
+        assertEquals(1, conferenceRoom.getGuestList().size() );
+    }
+
+    @Test
+    public void checkOutConferenceRoom(){
+        hotel.checkInConferenceRoom(guest, conferenceRoom);
+        assertEquals(1, conferenceRoom.getGuestList().size() );
+        hotel.checkOutConferenceRoom(conferenceRoom);
+        assertEquals(0, conferenceRoom.getGuestList().size() );
+    }
+
+    @Test
+    public void clearConferenceRoom(){
+        hotel.checkInConferenceRoom(guest, conferenceRoom);
+        hotel.checkInConferenceRoom(guest, conferenceRoom);
+        hotel.checkInConferenceRoom(guest, conferenceRoom);
+        assertEquals(3, conferenceRoom.getGuestList().size() );
+        hotel.clearConferenceRoom(conferenceRoom);
+        assertEquals(0, conferenceRoom.getGuestList().size() );
+    }
 }
