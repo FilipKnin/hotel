270d48f4a6c38f04306186a3221a32a68b29c949
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-10 11:02:22 +0100

    finishes dining room

--- src/main/java/DiningRoom.java
+++ src/main/java/DiningRoom.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+import java.util.BitSet;
+
 public class DiningRoom {
+    private String name;
+    private int capacity;
+    private ArrayList<Guest> guests;
+
+     public DiningRoom(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+        this.guests = new ArrayList<>();
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public ArrayList<Guest> getGuestList() {
+        return guests;
+    }
+
+    public void addGuest(Guest guest) {
+        guests.add(guest);
+    }
+
+    public void clearRoom() {
+        guests.clear();
+    }
+
+    public Guest removeGuest() {
+        return guests.remove(0);
+    }
 }
--- src/test/java/DiningRoomTest.java
+++ src/test/java/DiningRoomTest.java
@@ -1,2 +1,54 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DiningRoomTest {
+    private DiningRoom diningRoom;
+    private Guest guest;
+
+    @Before
+    public void beforeEach(){
+        diningRoom = new DiningRoom("The Refectory", 25);
+        guest = new Guest();
+    }
+
+    @Test
+    public void getRoomName(){
+        assertEquals("The Refectory", diningRoom.getName() );
+    }
+
+    @Test
+    public void getCapacity(){
+        assertEquals(25, diningRoom.getCapacity() );
+    }
+
+    @Test
+    public void getGuests(){
+        assertEquals(0, diningRoom.getGuestList().size() );
+    }
+
+    @Test
+    public void addGuest(){
+        diningRoom.addGuest(guest);
+        assertEquals(1, diningRoom.getGuestList().size() );
+    }
+
+    @Test
+    public void clearRoom(){
+        diningRoom.addGuest(guest);
+        diningRoom.addGuest(guest);
+        assertEquals(2, diningRoom.getGuestList().size() );
+        diningRoom.clearRoom();
+        assertEquals(0, diningRoom.getGuestList().size() );
+    }
+
+    @Test
+    public void removeGuest(){
+        diningRoom.addGuest(guest);
+        diningRoom.addGuest(guest);
+        assertEquals(2, diningRoom.getGuestList().size() );
+        diningRoom.removeGuest();
+        assertEquals(1, diningRoom.getGuestList().size() );
+    }
 }
